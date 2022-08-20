67ef7d5cf4823951a1d269ba27d6715c15189bad
Allan <allanchiang24@gmail.com>
2022-08-20 16:00:50 +1200

    Add function to return random value of rock, paper or scissors

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+const rockPaperScissors = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    const computerChoice = rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
