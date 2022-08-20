3cdce0d42061b4ef8190bd8616d452b5a870aada
Allan <allanchiang24@gmail.com>
2022-08-20 16:20:13 +1200

    Add more if statements to further rock papes scissors game and add toLowerCase() to prompt

--- js/script.js
+++ js/script.js
@@ -8,9 +8,13 @@ function getComputerChoice() {
 function playRockPaperScissors(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'rock') {
         return "It's a draw!";
-    } 
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return "You win! Rock beats scissors."
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return "You lose! Paper beats rock."
+    }
 }
 
-const playerSelection = prompt("Rock, paper or scissors?");
+const playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
 const computerSelection = getComputerChoice();
 console.log(playRockPaperScissors(playerSelection, computerSelection));
