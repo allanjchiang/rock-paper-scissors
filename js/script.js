31bd7c831234ac0c51d879d1ed11e17f522fb1d3
Allan <allanchiang24@gmail.com>
2023-03-09 13:18:25 +1300

    Add console.log for computerChoice

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const rockPaperScissors = ['rock', 'paper', 'scissors'];
 function getComputerChoice() {
     const computerChoice = rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
     return computerChoice;
+    console.log(computerChoice);
 }
 
 function playRockPaperScissors(playerSelection, computerSelection) {
