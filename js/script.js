f5651ec26d9dca812aec5079aed7c2b375b2c696
Allan <allanchiang24@gmail.com>
2023-03-10 16:20:14 +1300

    Change win and lose variables to 0

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ function getComputerChoice() {
 }
 
 function playRockPaperScissors(playerSelection, computerSelection) {
-    let win;
-    let lose;
+    let win = 0;
+    let lose = 0;
     if (playerSelection === 'rock' && computerSelection === 'rock') {
         return "It's a draw!";
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
