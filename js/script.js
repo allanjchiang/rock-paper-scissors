974412638d0041097002c0ed72a4a5926f46897e
Allan <allanchiang24@gmail.com>
2023-03-10 15:57:57 +1300

    Add console.log including the return statements

--- js/script.js
+++ js/script.js
@@ -13,26 +13,34 @@ function playRockPaperScissors(playerSelection, computerSelection) {
         return "It's a draw!";
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         win++;
-        return "You win! Rock beats scissors."
+        return "You win! Rock beats scissors.";
+        console.log("You win! Rock beats scissors.");
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         lose++;
-        return "You lose! Paper beats rock."
+        return "You lose! Paper beats rock.";
+        console.log("You lose! Paper beats rock.");
     } else if (playerSelection === 'paper' && computerSelection === 'paper') {
         return "It's a draw!";
+        console.log("It's a draw!");
     } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
         return "It's a draw!";
+        console.log("It's a draw!");
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         lose++;
         return "You lose! Scissors beats paper.";
+        console.log("You lose! Scissors beats paper.");
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         win++;
         return "You win! Paper beats rock.";
+        console.log("You win! Paper beats rock.");
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         lose++;
         return "You lose! Rock beats scissors";
+        console.log("You lose! Rock beats scissors");
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         win++;
         return "You win! Scissors beats paper.";
+        console.log("You win! Scissors beats paper.");
     }
 }
 
