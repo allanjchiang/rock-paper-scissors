43bb4eb91e2c0c1e271bf999af379470038e03bc
Allan <allanchiang24@gmail.com>
2023-03-10 16:24:19 +1300

    Add linebreak after user's score is console.logged

--- js/script.js
+++ js/script.js
@@ -50,11 +50,11 @@ function game() {
         console.log(playRockPaperScissors(playerSelection, computerSelection));
     }
     if (win > lose) {
-        console.log("You won, your score was: " + win + " and the computer's score was: " + lose + ".");
+        console.log("You won, your score was: " + win + "\n and the computer's score was: " + lose + ".");
     } else if (win < lose) {
-        console.log("You lost, your score was: " + win + " and the computer's score was: " + lose + ".");
+        console.log("You lost, your score was: " + win + "\n and the computer's score was: " + lose + ".");
     } else if (win === lose) {
-        console.log("You tied! Your score was: " + win + " and the computer's score was: " + lose + ".");
+        console.log("You tied! Your score was: " + win + "\n and the computer's score was: " + lose + ".");
     }
 }
 
