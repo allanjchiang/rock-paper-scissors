16a1b65aae714cf3fd1b81fa960a7af1f37b0e4d
Allan Chiang <allanchiang24@gmail.com>
2023-12-08 21:39:17 +1300

    Add result = getResult

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ let playerScore = 0;
 let computerScore = 0;
 
 
-function playRockPaperScissors(playerSelection) {
+function playRockPaperScissors(playerChoice) {
     const choices = ['rock', 'paper', 'scissors'];
     const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+    const result = getResult(playerChoice, computerChoice);
 }
 
 
