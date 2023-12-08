dcd639e08a5f95faea7b1094eeb28eb566fa05de
Allan Chiang <allanchiang24@gmail.com>
2023-12-08 21:39:42 +1300

    Move event listener clicks to top of code

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
 
-
-function playRockPaperScissors(playerChoice) {
-    const choices = ['rock', 'paper', 'scissors'];
-    const computerChoice = choices[Math.floor(Math.random() * 3)];
-
-    const result = getResult(playerChoice, computerChoice);
-}
-
-
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
     playRockPaperScissors(rock);
@@ -24,6 +15,17 @@ const scissors = document.querySelector('#scissors');
 scissors.addEventListener('click', () => {
     playRockPaperScissors(scissors);
 });
+
+
+function playRockPaperScissors(playerChoice) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+    const result = getResult(playerChoice, computerChoice);
+}
+
+
+
 
 function game() {
     
